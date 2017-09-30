6a66decc4dfe8eac2816e5f1ac24b28a0b01bfa3
Sambo68 <iamireh@aol.com>
2017-09-30 14:13:09 -0400

    updates

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,10 @@ var mysql = require('mysql');
 var connection;
 
 if(process.env.JAWSDB_URL) {
+    //Heroku deployment
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
+  //local host
     connection = mysql.createConnection({
         root: 3000,
         host: 'localhost',
